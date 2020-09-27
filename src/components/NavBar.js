4f2b09ae9a43e58dd510e946100d7b3408fcee44
NB-famous <ncbadoles@gmail.com>
2020-09-27 13:22:08 -0400

    modified styling on sidebar navigation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,37 +22,55 @@ const NavBar = (props) => {
         localStorage.removeItem("cleanerEmail");
     }
 
+
   return (
     <section className="sidebar">
-                <Link to={'/'}>
-                <img
-                    className="sidebar--centered"
-                    src={logo}
-                    alt="cleaneast"
-                />
-                </Link>
-                <hr className="sidebar__separator sidebar--centered" />
-                <nav className="sidebar__menu">
-                  { 
-                      loggedIn ? 
-                      <ul>
-                      <button className="list-group-item list-group-item-action" onClick={handleLogout}>
-                      <h1><strong>Logout</strong></h1>
-                      </button>
-                      </ul>
-                      :
-                      cleanerLogin ?
-                      <ul> 
-                      <button className="list-group-item list-group-item-action" onClick={handleCleanerLogout }>
-                      <h1><strong>Logout</strong></h1>
-                      </button>
-                      </ul>
-                      :
-                      <ul>
-                      <ButtonList/>
-                      </ul>
-                  }  
-                </nav>
+      <Link to={'/'}>
+      <img
+          className="sidebar--centered"
+          src={logo}
+          alt="cleaneast"
+      />
+      </Link>
+      <hr className="sidebar__separator sidebar--centered" />
+        { 
+            loggedIn ?
+            <nav className="sidebar__user"> 
+            <ul>
+            <button className="list-group-item list-group-item-action" onClick={handleLogout}>
+            <h1><strong>Logout</strong></h1>
+            </button>
+            </ul>
+            </nav>
+            :
+            cleanerLogin ?
+            <nav className="sidebar__cleaner">
+            <ul>
+            <li>
+            <button className="list-group-item list-group-item-action" >
+            <h1><strong>Profile</strong></h1>
+            </button>
+            </li>
+            <li>
+            <button className="list-group-item list-group-item-action" >
+            <h1><strong>Inbox</strong></h1>
+            </button>
+            </li>
+            <li>
+            <button className="list-group-item list-group-item-action" onClick={handleCleanerLogout }>
+            <h1><strong>Logout</strong></h1>
+            </button>
+            </li>
+            </ul>
+            </nav>
+            :
+            <nav className="sidebar__menu">
+            <ul>
+            <ButtonList/>
+            </ul>
+            </nav>
+        }  
+              
     </section>
   )
 }
