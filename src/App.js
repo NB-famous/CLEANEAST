8dbbd7fb7d5497648410fc3cccbf625d81f4a299
NB-famous <ncbadoles@gmail.com>
2020-10-16 16:06:09 -0400

    trying to fix api axios calls

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css'
 //import Navigation from "./components/Navigation"
 import Application from "./components/Application"
+
 /* import axios from 'axios';
 //axios.defaults.baseURL = 'http://localhost:3000'
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
-//axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'http://localhost:3000'
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
